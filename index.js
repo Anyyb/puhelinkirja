5d9e4d8b9fae6f9d3a37215d0490aec427feb099
Anyyb <anusalminen1@windowslive.com>
2024-07-03 15:37:38 +0300

    puhelinluettelo ja tietokanta step 3,4,5,6
    Tehtävät 3.15.-3.18.
    step 3,4,5,6

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ app.use(cors())
 app.use(morgan('tiny'))// käytetään morgan tinya ja näytetään tinyn sisältämät pyynnön perustiedot.
 app.use(morgan(':body'))// asetetaan moran käyttämään luotua tokenia ja näyttämään tiedot.
 
-// huomioni: tiedot tulostuivat konsoliin allekkain eivätkä saman tiny formaatin perään, 
-// koska olen asettanut sovelluksen käyttämään molempia erikseen. Kokeilin yhdistää, mutta ei toiminut.
-// POST /api/persons 200 50 - 3.807 ms
-//{"name":"John Snow","number":"040321408"}
 
 //luodaan morgan token nimellä body ja palautetaan pyynnöstä saatu data JSON muodossa (JSON.stringify:llä).
 morgan.token('body', function(request, response ) { 
@@ -26,7 +22,7 @@ app.get('/',(request, response) => {
   response.send('<h1>Front Page</h1>')
 })
 // http get pyynnöt polkuun persons
-//haetaan tietokannasta collectionista Persons
+//haetaan tiedot tietokannan collectionista Persons
 app.get('/api/persons',(request, response) => {
   Person.find({}).then(result => {
     response.json(result)  
@@ -50,15 +46,18 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
-// numeron poisto tietokannasta
-app.delete('/api/persons/:number', (request, response) => {
-  Person.findOneAndDelete({ number:request.params.number}).then(result => {
-  response.status(204).end()
-})
+
+//henkilön poisto ID:n avulla
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // henkilön lisäys, tehdään post pyyntö polkuun /api/persons
-app.post('/api/persons',(request,response) => {
+app.post('/api/persons',(request,response,next) => {
     const body=request.body
 
     // käsitellään jos pyynnöstä puuttuu nimi tai numero
@@ -68,15 +67,51 @@ app.post('/api/persons',(request,response) => {
         error: 'name and number cannot be empty'
       })
     }
+    
     const person = new Person({
       name: body.name,
       number:body.number,
     })
+    
     //lisätään henkilö tietokantaan
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
+//henkilön tietojen päivitys
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number:body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+//virheidenkäsittelyn middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  if (error.name === 'BadRequest') {
+    return response.status(400).send({ error: 'bad request'})
+  }
+  if (error.name === 'NoContent') {
+    return response.status(204).send({ error: 'noContent'})
+  }
+  if (error.name === 'ServerError') {
+    return response.status(500).json({ error: 'Internal Server Error'})
+  }
+
+  next(error)
+}
 
 //portti .env tiedostosta 
 const PORT = process.env.PORT;
@@ -86,4 +121,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/api/persons`);
   console.log(`Server is running on http://localhost:${PORT}/info`);
   console.log(`Server is running on http://localhost:${PORT}/api/persons/667b1fa95f3382bac6464b30`);
-});
+})
+app.use(errorHandler)
